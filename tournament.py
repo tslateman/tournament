6c5d0454af32dd439e4c92b4bb13653fb1b4adf5
tslateman <t.r.slater@gmail.com>
2015-09-08 10:27:39 -0700

    Add myCursor function to further reduce code repeating
    Add more comments

--- tournament.py
+++ tournament.py
@@ -4,40 +4,53 @@
 #
 
 import psycopg2
+import contextlib
+
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
     try:
-        db = psycopg2.connect("dbname=tournament")
-        cursor = db.cursor()
-        return db, cursor
+        conn = psycopg2.connect("dbname=tournament")
+        return conn
     except:
         print("Database Connection Error")
 
 
+@contextlib.contextmanager
+def myCursor():
+    """Function to consolidate db transaction code."""
+    conn = connect()
+    cursor = conn.cursor()
+    try:
+        yield cursor
+    except:
+        print("Database Cursor Error") 
+    else:
+        conn.commit()
+    finally:
+        cursor.close()
+        conn.close()
+
+
 def deleteMatches():
     """Remove all the match records from the database."""
-    db, cursor = connect()
-    cursor.execute("DELETE FROM matches;")
-    db.commit()
-    db.close()
+    with myCursor() as cursor:
+        cursor.execute("TRUNCATE matches;")
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
-    db, cursor = connect()
-    cursor.execute("DELETE FROM players;")
-    db.commit()
-    db.close()
+    with myCursor() as cursor:
+        cursor.execute("DELETE FROM players;")
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
-    db, cursor = connect()
-    cursor.execute("SELECT count(p.id) FROM players p WHERE p.id != '-404';")
-    result = cursor.fetchone()
+    with myCursor() as cursor:
+        cursor.execute("SELECT count(p.id) FROM players p \
+            WHERE p.id != '-404';")
+        result = cursor.fetchone()
     count = result[0]
-    db.close()
     return count
 
 
@@ -51,12 +64,11 @@ def registerPlayer(name):
       name: the player's full name (need not be unique).
     """
 
-    db, cursor = connect()
-    query = "INSERT INTO players (name) VALUES (%s);"
-    parameter = (name,)
-    cursor.execute(query, parameter)
-    db.commit()
-    db.close()
+    with myCursor() as cursor:
+        query = "INSERT INTO players (name) VALUES (%s);"
+        parameter = (name,)
+        cursor.execute(query, parameter)
+
 
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
@@ -72,10 +84,9 @@ def playerStandings():
         matches: the number of matches the player has played
     """
 
-    db, cursor = connect()
-    cursor.execute("SELECT id, name, wins, mcount FROM standings ")
-    standings = cursor.fetchall()
-    db.close()
+    with myCursor() as cursor:
+        cursor.execute("SELECT id, name, wins, mcount FROM standings ")
+        standings = cursor.fetchall()
     return standings
 
 
@@ -86,31 +97,28 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
-    db, cursor = connect()
-    query = "INSERT INTO matches (winner_id, loser_id) values (%s, %s);"
-    parameters = (winner, loser)
-    cursor.execute(query, parameters)
-    db.commit()
-    db.close()
+    with myCursor() as cursor:
+        query = "INSERT INTO matches (winner_id, loser_id) values (%s, %s);"
+        parameters = (winner, loser)
+        cursor.execute(query, parameters)
 
 
 def reportBye(bye_player):
     """Registers a bye for a given player. Bye id assigned as '-404'.
+    Updates both players table and matches table.
 
     Args:
       bye_player: player id receiving a bye.
 
     """
 
-    db, cursor = connect() 
-    query = "INSERT INTO players (id, name) VALUES ('-404','bye');"
-    cursor.execute(query)
-    
-    query = "INSERT INTO matches (winner_id, loser_id) values ('%s', '-404');"
-    parameter = (bye_player,)
-    cursor.execute(query, parameter)
-    db.commit()
-    db.close()
+    with myCursor() as cursor:
+        query = "INSERT INTO players (id, name) VALUES ('-404','bye');"
+        cursor.execute(query)    
+        query = "INSERT INTO matches (winner_id, loser_id) \
+                values ('%s', '-404');"
+        parameter = (bye_player,)
+        cursor.execute(query, parameter)
 
 
 def hasByes(player_id):
@@ -122,12 +130,11 @@ def hasByes(player_id):
     Returns: count of byes of player
     """
 
-    db, cursor = connect()
-    query = "SELECT count(id) FROM matches WHERE winner_id = %s \
+    with myCursor() as cursor:
+        query = "SELECT count(id) FROM matches WHERE winner_id = %s \
                         and loser_id ='-404';"
-    parameter = (player_id,)
-    byecount = cursor.execute(query, parameter)
-    db.close()
+        parameter = (player_id,)
+        byecount = cursor.execute(query, parameter)
     return byecount
 
 
@@ -157,6 +164,7 @@ def swissPairings():
     if len(players) % 2 != 0:
         bye_index = len(players) - 1
         bye_player = players.pop()
+        # Identify player without byes to assign a bye to
         while (hasByes(bye_player[0]) > 0 and bye_index > 0):
             players.insert(bye_index, byePlayer)
             bye_index -= 1
@@ -164,6 +172,7 @@ def swissPairings():
         pairings.insert(
             bye_index, (bye_player[0], bye_player[1], '-404', 'bye'))
 
+    # Assign players pairings
     while (len(players) > 1):
         player1 = players.pop()
         player2 = players.pop()
